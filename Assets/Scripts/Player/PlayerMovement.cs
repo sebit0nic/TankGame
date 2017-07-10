c4da015920d1aa9dcce736c71fe9f0db2e5c1bb0
sebit0nic <sebastian.lackner7@gmail.com>
2017-07-10 15:54:12 +0200

    Added weapon cooldown

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,28 +6,25 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float movementSpeed = 1;
 	public float rotationSpeed = 1;
-	public float projectileSpeed = 500f;
 	public Transform body;
 	public Transform barrel;
 	public Transform barrelEnd;
-	public Rigidbody projectile;
 
+	private PlayerShootingScript playerShootingScript;
 	private Rigidbody thisRigidbody;
-	private LineRenderer thisLineRenderer;
 	private int floorMask;
 	private float camRayLength = 100f;
 
 	private void Awake() {
+		playerShootingScript = GetComponent<PlayerShootingScript> ();
 		thisRigidbody = GetComponent<Rigidbody> ();
-		thisLineRenderer = GetComponent<LineRenderer> ();
 
 		floorMask = LayerMask.GetMask ("Floor");
 	}
 
 	private void Update() {
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			Rigidbody newProjectile = Instantiate (projectile, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
-			newProjectile.AddForce (newProjectile.transform.forward * projectileSpeed, ForceMode.Force);
+			playerShootingScript.Shoot ();
 		}
 	}
 
@@ -57,8 +54,8 @@ public class PlayerMovement : MonoBehaviour {
 			playerToMouse.y = 0;
 			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
 			barrel.rotation = newRotation;
-			thisLineRenderer.SetPosition (0, new Vector3 (barrelEnd.position.x, barrelEnd.position.y, barrelEnd.position.z));
-			thisLineRenderer.SetPosition (1, new Vector3 (floorHit.point.x, barrelEnd.position.y, floorHit.point.z));
+
+			playerShootingScript.UpdateTargetingLine (floorHit);
 		}
 	}
 }
---
+++ Assets/Scripts/Player/PlayerShootingScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShootingScript : MonoBehaviour {
+
+	public Transform barrelEnd;
+	public float projectileSpeed = 500f;
+	public float weaponCooldown = 1f;
+	public Rigidbody simpleMissileTemplate;
+	public enum WeaponType {simpleMissile};
+	public WeaponType currentWeaponType;
+	public Image cooldown;
+
+	private LineRenderer thisLineRenderer;
+	private float shootTimer;
+	private float fillAmountTimer = 1f;
+
+	private void Awake() {
+		thisLineRenderer = GetComponent<LineRenderer> ();
+	}
+
+	private void Update() {
+		float fillAmount = weaponCooldown - fillAmountTimer;
+		if (fillAmount >= 1) {
+			cooldown.fillAmount = 1;
+		} else {
+			cooldown.fillAmount = fillAmount;
+		}
+		fillAmountTimer += Time.deltaTime;
+	}
+
+	public void Shoot() {
+		if (shootTimer < Time.time) {
+			switch (currentWeaponType) {
+			case WeaponType.simpleMissile:
+				Rigidbody newProjectile = Instantiate (simpleMissileTemplate, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+				newProjectile.AddForce (newProjectile.transform.forward * projectileSpeed, ForceMode.Force);
+				break;
+			}
+			shootTimer = Time.time + weaponCooldown;
+			fillAmountTimer = 0.01f;
+		}
+	}
+
+	public void UpdateTargetingLine(RaycastHit floorHit) {
+		thisLineRenderer.SetPosition (0, new Vector3 (barrelEnd.position.x, barrelEnd.position.y, barrelEnd.position.z));
+		thisLineRenderer.SetPosition (1, new Vector3 (floorHit.point.x, barrelEnd.position.y, floorHit.point.z));
+	}
+}
