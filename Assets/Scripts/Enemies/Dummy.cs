24058365a4394e25d16a4ba31135251e1a46b925
sebit0nic <sebastian.lackner7@gmail.com>
2017-08-06 13:03:53 +0200

    Added dummies and instructions for tutorial level

---
+++ Assets/Scripts/Enemies/Dummy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour, Enemy {
+
+	private GameManager gameManager;
+
+	private void Awake() {
+		gameManager = GameObject.Find ("Game Manager").GetComponent<GameManager> ();
+	}
+
+	public void HitByProjectile() {
+		//TODO: play destroyed animation/particle effect
+		gameManager.NotifyEnemyDestroyed (0);
+		gameObject.SetActive (false);
+	}
+}
