6eb840f6f832ec7560a103481bea16c2e4944c37
sebit0nic <sebastian.lackner7@gmail.com>
2017-08-06 16:14:22 +0200

    Pooled projectile particles

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
-	public GameObject projectileExplosion;
+	public ParticleSystem[] thisParticleSystem;
 
-	private GameObject instantiatedExplosion;
-	private ParticleSystem instantiatedParticleSystem;
 	private IEnumerator particleCoroutine;
 	private Rigidbody thisRigidbody;
 	private Collider thisCollider;
-	private Transform dynamicObjects;
 	private GameObject thisRenderer;
 
 	private void Awake() {
-		dynamicObjects = GameObject.Find ("Dynamic Objects").GetComponent<Transform>();
-		instantiatedExplosion = Instantiate (projectileExplosion, transform.position, projectileExplosion.transform.rotation) as GameObject;
-		instantiatedExplosion.transform.parent = dynamicObjects;
-		instantiatedParticleSystem = instantiatedExplosion.GetComponent<ParticleSystem> ();
-		instantiatedExplosion.SetActive (false);
 		thisRigidbody = GetComponent<Rigidbody> ();
 		thisCollider = GetComponent<Collider> ();
 		thisRenderer = transform.GetChild (0).gameObject;
 	}
 
 	public void OnProjectileDestroy() {
-		particleCoroutine = WaitForParticleFinish (instantiatedParticleSystem.main.duration);
+		particleCoroutine = WaitForParticleFinish (thisParticleSystem[0].main.duration);
 
 		thisRenderer.SetActive (false);
 		thisRigidbody.isKinematic = true;
 		thisCollider.enabled = false;
-		instantiatedExplosion.transform.position = transform.position;
-		instantiatedExplosion.SetActive (true);
-		instantiatedParticleSystem.Play ();
+		for (int i = 0; i < thisParticleSystem.Length; i++) {
+			thisParticleSystem [i].Play ();
+		}
 		StartCoroutine (particleCoroutine);
 	}
 
 	private IEnumerator WaitForParticleFinish(float waitTime) {
 		yield return new WaitForSeconds (waitTime);
-		instantiatedExplosion.SetActive (false);
-		instantiatedParticleSystem.Clear ();
+		for (int i = 0; i < thisParticleSystem.Length; i++) {
+			thisParticleSystem [i].Clear ();
+		}
 		thisRenderer.SetActive (true);
 		thisRigidbody.isKinematic = true;
 		gameObject.SetActive (false);
