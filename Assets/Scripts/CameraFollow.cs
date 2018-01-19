0df8e2386928aa9782957143f084075058adc15f
sebit0nic <sebastian.lackner7@gmail.com>
2018-01-19 21:54:14 +0100

    Added colliders for mountains

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	public float smoothing = 3f;
 	public float mouseDamper = 0.5f;
+	public Vector3 minCameraBoundaries, maxCameraBoundaries;
 
 	private Vector3 offset;
 	private int floorMask;
@@ -39,6 +40,11 @@ public class CameraFollow : MonoBehaviour {
 		} else {
 			targetCamPos = target.position + offset;
 		}
+
+		Vector3 tempPos = targetCamPos;
+		tempPos.x = Mathf.Clamp (tempPos.x, minCameraBoundaries.x, maxCameraBoundaries.x);
+		tempPos.z = Mathf.Clamp (tempPos.z, minCameraBoundaries.z, maxCameraBoundaries.z);
+		targetCamPos = tempPos;
 
 		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 	}
