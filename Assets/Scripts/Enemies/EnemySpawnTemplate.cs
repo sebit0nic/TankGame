8da72e5d2d4745f5eaa4f2c9244e48f1690e47a9
sebit0nic <sebastian.lackner7@gmail.com>
2018-01-19 20:02:03 +0100

    Added spawn edge

---
+++ Assets/Scripts/Enemies/EnemySpawnTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemySpawnTemplate {
+	
+	//0 = Normal Missile Enemy, 1 = Exploding Enemy, 2 = Rotating Enemy
+	public int[] spawnedEnemyType;
+	public int[] affectedSpawners;
+	public int[] spawnedAmount;
+}
